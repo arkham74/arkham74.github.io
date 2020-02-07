4ea93a26b0ade87c4ce062bdd9a7105e962684fb
Arkham <arkham195@gmail.com>
2020-02-07 22:26:19 +0100

    things

---
+++ js/script.js
@@ -0,0 +1,5 @@
+$( document ).ready(function() {
+	$("#include_header").load("html/header.html");
+	$("#include_content").load("html/content.html");
+	$("#include_footer").load("html/footer.html");
+});
