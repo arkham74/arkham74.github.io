97a4b9382af4480154763aa031e332ac8bb7508d
JD <arkham195@gmail.com>
2022-08-18 13:21:31 +0200

    new website

--- js/script.js
+++
@@ -1,5 +0,0 @@
-$( document ).ready(function() {
-	$("#include_header").load("html/header.html");
-	$("#include_content").load("html/content.html");
-	$("#include_footer").load("html/footer.html");
-});
